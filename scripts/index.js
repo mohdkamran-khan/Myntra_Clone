073e2bb11e381c1c5a200aad6c1be89909408608
Mohd Kamran Khan <khanmohdkamran@yahoo.com>
2025-06-24 13:04:03 +0530

    Upload

---
+++ scripts/index.js
@@ -0,0 +1,54 @@
+function onload() {  
+  let bagItemsStr= localStorage.getItem("bagItems");
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];  
+  displayItemsOnHome();
+  displayBagIcon();
+}
+
+function displayItemsOnHome(){
+let itemsContainerElement = document.querySelector(".items-container");
+if(itemsContainerElement === null) {
+    return;
+}
+let innerHTML= '';
+items.forEach(item => {
+    innerHTML +=  `
+<div class="item-container">
+                <img class="item-image" src="${item.image}" alt="Item Image">
+                <div class="rating">
+                    ${item.rating.stars} ⭐| ${item.rating.count}
+                </div>
+                <div class="company-name">
+                    ${item.company}
+                </div>
+                <div class="item-name">${item.item_name}</div>
+                <div class="price">
+                    <span class="current-price">₹${item.current_price}</span>
+                    <span class="original-price">₹${item.original_price}</span>
+                    <span class="discount">(${item.discount_percentage}% OFF)</span>
+                </div>
+                <button class="btn-add-bag" onclick="addToBag('${item.id}')">Add to Bag</button>
+            </div>`;
+});
+
+itemsContainerElement.innerHTML = innerHTML;
+}
+
+let bagItems;
+
+function addToBag(itemId) {
+    bagItems.push(itemId);
+    localStorage.setItem("bagItems", JSON.stringify(bagItems));
+    displayBagIcon();
+}
+onload();
+
+function displayBagIcon() {
+    let bagItemCountElement = document.querySelector(".item-count");
+    if(bagItems.length > 0) {
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = `${bagItems.length}`;
+    }else{
+    bagItemCountElement.style.visibility = 'hidden';
+    }
+}
